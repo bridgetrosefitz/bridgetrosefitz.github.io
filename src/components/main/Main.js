6f67cb870cf673ecb843e26a3677c42b49fec6c8
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-07-05 17:36:48 -0400

    Adds company logos; plays with company logo CSS (and some other CSS)

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,7 @@
 import './main.css'
 import { HiOutlineArrowNarrowDown } from 'react-icons/hi'
 
+// NavArrow component
 const NavArrow = () => {
 
   return ( 
@@ -10,11 +11,26 @@ const NavArrow = () => {
   )
 }
 
+// Main component
+//  On company logos, make onHover go back to normal color on company logos
+//  On company logos, make onClick go to website
+
 const Main = () => {
 
-
   return (
-    <NavArrow/>
+    <>
+
+      <div className='image-display-container'>
+        <img alt='Shadow' src='company-logos/shadow-logo-red.png'></img>
+        <img alt='LEK Consulting' src='company-logos/lek-logo.svg'></img>
+        <img alt='World Economic Forum' src='company-logos/wef-logo.png'></img>
+        <img alt='国際交流基金' src='company-logos/japan-foundation-logo-english.png'></img>
+        <img alt='Institut Montaigne' src='company-logos/institut-montaigne-logo.png'></img>
+        <img alt='United Nations' src='company-logos/united-nations-logo.png'></img>
+        <img alt='Australian Music Examinations Board' src='company-logos/ameb-logo.jpeg'></img>
+      </div>
+      <NavArrow />
+    </>
   )
 }
 
