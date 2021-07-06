c910f8ad2c482f8aeb702677e0e6bead0a79e21c
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-07-06 18:00:40 -0400

    Adds inari-temple and times-square encironment textures

--- src/components/spheres/Spheres.js
+++ src/components/spheres/Spheres.js
@@ -10,10 +10,12 @@ const debugObject = {
 
 export const Spheres = () => {
 
+  const sphere = useRef()
+  const numberOfSpheres = 1
   // This link was somewhat helpful to get the texture loaded https://codeworkshop.dev/blog/2020-06-14-creating-a-skybox-with-reflections-in-react-three-fiber/
   const loader = new CubeTextureLoader()
 
-  const environmentMapTexture = loader.load([
+  const louvreEnvironmentMapTexture = loader.load([
     '/environments/louvre/px.png',
     '/environments/louvre/nx.png',
     '/environments/louvre/py.png',
@@ -22,22 +24,36 @@ export const Spheres = () => {
     '/environments/louvre/nz.png',
   ])
 
-  
-  const sphere1 = useRef()
-  const numberOfSpheres = 1;
+  const templeEnvironmentMapTexture = loader.load([
+    '/environments/inari-temple/px.png',
+    '/environments/inari-temple/nx.png',
+    '/environments/inari-temple/py.png',
+    '/environments/inari-temple/ny.png',
+    '/environments/inari-temple/pz.png',
+    '/environments/inari-temple/nz.png',
+  ])
+
+  const timesSquareEnvironmentMapTexture = loader.load([
+    '/environments/times-square/px.png',
+    '/environments/times-square/nx.png',
+    '/environments/times-square/py.png',
+    '/environments/times-square/ny.png',
+    '/environments/times-square/pz.png',
+    '/environments/times-square/nz.png',
+  ])
+
 
   const makeASphere = (index) => { 
     return (
     <mesh 
       key={index}
-      ref={sphere1}
-      rotation={[Math.PI * Math.random(), Math.PI * 0.1, Math.PI * 0.25]}
-      position={[(Math.random() - 0.5) * 50, 0, 0]}>
+      ref={sphere}
+      position={[0, 0, 0]}>
     <sphereGeometry args={[10, 100, 100]} />
     <meshStandardMaterial
       roughness={debugObject.roughness}
       metalness={debugObject.metalness}
-      envMap={environmentMapTexture}
+          envMap={timesSquareEnvironmentMapTexture}
     />
   </mesh>) 
   }
@@ -53,8 +69,8 @@ export const Spheres = () => {
       .max(1)
       .step(0.001)
       .onFinishChange(() => {  
-        sphere1.current.material.metalness = debugObject.metalness
-        sphere1.current.material.needsUpdate = true
+        sphere.current.material.metalness = debugObject.metalness
+        sphere.current.material.needsUpdate = true
       })
 
     gui.add(debugObject, 'roughness')
@@ -62,8 +78,8 @@ export const Spheres = () => {
       .max(1)
       .step(0.001)
       .onFinishChange(() => {
-        sphere1.current.material.roughness = debugObject.roughness
-        sphere1.current.material.needsUpdate = true
+        sphere.current.material.roughness = debugObject.roughness
+        sphere.current.material.needsUpdate = true
       })
   })
 
