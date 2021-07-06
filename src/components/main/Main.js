3d2b38863220fdef19bfd73c869cc9cd464e71bc
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-07-05 20:29:36 -0400

    Creates projects grid; adds Roboto fonts

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -12,22 +12,40 @@ const NavArrow = () => {
 }
 
 // Main component
-//  On company logos, make onHover go back to normal color on company logos
 //  On company logos, make onClick go to website
 
 const Main = () => {
 
   return (
-    <>
-
-      <div className='image-display-container'>
+    <>  
+      <div className='company-logo-display-container'>
         <img alt='Shadow' src='company-logos/shadow-logo-red.png'></img>
         <img alt='LEK Consulting' src='company-logos/lek-logo.svg'></img>
         <img alt='World Economic Forum' src='company-logos/wef-logo.png'></img>
-        <img alt='国際交流基金' src='company-logos/japan-foundation-logo-english.png'></img>
-        <img alt='Institut Montaigne' src='company-logos/institut-montaigne-logo.png'></img>
         <img alt='United Nations' src='company-logos/united-nations-logo.png'></img>
+        <img alt='Institut Montaigne' src='company-logos/institut-montaigne-logo.png'></img>
+        <img alt='国際交流基金' src='company-logos/japan-foundation-logo-english.png'></img>
         <img alt='Australian Music Examinations Board' src='company-logos/ameb-logo.jpeg'></img>
+      </div>
+      <div className='projects-display-container'>
+        <div className="grid-item">
+            <img alt='Digital Innovation Statistics App' src='project-snapshots/digital-innovation.png'></img>
+            <div>Digital Innovation Statistics</div>
+          </div>
+          <div className="grid-item">
+            <img alt='RACI App' src='project-snapshots/raci.png'></img>
+            <div>RACI Project Management</div>
+          </div>
+          <div className="grid-item">
+            <img alt='Particles' src='project-snapshots/particles.png'></img>
+            <div>Particles - Three.js</div>
+          </div>
+          <div className="grid-item">
+            <img alt='Food Systems Dialogues' src='project-snapshots/food-systems-dialogues.png'></img>
+          <div>Food Systems Dialogues <span className='small-text'>(Product managed)</span></div>
+          </div>
+          <div className="grid-item"></div>
+          <div className="grid-item"></div>
       </div>
       <NavArrow />
     </>
