a9db474a6bb09746a2f0cf784ec978aeeba7f28c
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-07-05 23:35:04 -0400

    Adds texture to sphere; creates multiple spheres; adds Suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react'
+import React, {Suspense, useRef} from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Header from './components/header/Header'
@@ -9,17 +9,19 @@ function App() {
   const directionalLight = useRef()
   return (
     <div>
-      <Header />
-      <div id="canvas-container"/>
-      <Canvas
-        camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 30] }}>
-        <OrbitControls/>
-        <ambientLight intensity={0.1} />
-        <directionalLight ref={directionalLight} color="red" position={[0, 5, 5]} />
-        {directionalLight.current && <directionalLightHelper ref={[directionalLight, 5]}/>}
-        <Spheres/>
-      </Canvas>
-      <Main />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Header />
+        <div id="canvas-container"/>
+        <Canvas
+          camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 30] }}>
+          <OrbitControls/>
+          <ambientLight intensity={0.1} />
+          <directionalLight ref={directionalLight} position={[0, 5, 5]} />
+          {directionalLight.current && <directionalLightHelper ref={[directionalLight, 5]}/>}
+          <Spheres/>
+        </Canvas>
+        <Main />
+      </Suspense>
     </div>
   );
 }
--- src/components/spheres/Spheres.js
+++ src/components/spheres/Spheres.js
@@ -5,13 +5,39 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
 
 const Spheres = () => {
-  // const [colorMap] = useLoader(TextureLoader, ['PavingStones092_1K_Color.jpg'])
+  const [
+    colorMap, 
+    ambientOcclusionMap, 
+    displacementMap, 
+    normalMap, 
+    roughnessMap] = useLoader(TextureLoader, ['Tiles105_1K_Color.png', 'Tiles105_1K_AmbientOcclusion.png', 'Tiles105_1K_Displacement.png', 'Tiles105_1K_Normal.png', 'Tiles105_1K_Roughness.png'])
+
+  const numberOfSpheres = 4;
+
+
+
+  const makeASphere = () => { 
+    return (
+    <mesh 
+      rotation={[Math.PI * Math.random(), Math.PI * 0.1, Math.PI * 0.25]}
+      position={[(Math.random() - 0.5) * 50, 0, 0]}>
+    <sphereGeometry args={[4, 100, 100]} />
+    <meshStandardMaterial
+      map={colorMap}
+      displacementMap={displacementMap}
+      normalMap={normalMap}
+      roughnessMap={roughnessMap}
+      aoMap={ambientOcclusionMap} />
+  </mesh>) 
+  }
+
+  
+  const multipleSpheres = new Array(numberOfSpheres).fill(0).map(() => makeASphere())
 
   return (
-    <mesh>
-      <sphereGeometry args={[8, 32, 32]}/>
-      <meshPhongMaterial />
-    </mesh>
+    <group>
+      {multipleSpheres}
+    </group>
   )
 }
 
