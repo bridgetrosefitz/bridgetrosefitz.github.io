7050508672f401c039c78489b0b4a012efdbee8c
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-07-07 14:55:53 -0400

    Sets up project.text props passing into ProjectSpecs

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -10,14 +10,7 @@ const Project = ({hidden, onClose, project}) => {
       <h1>{project.title}
         <IoCloseOutline className='close-icon' onClick={onClose} />
       </h1>
-      <ProjectSpecs/>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
-      in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <ProjectSpecs project={project}/>
     </div>
   )
 }
--- src/components/project/ProjectSpecs.js
+++ src/components/project/ProjectSpecs.js
@@ -8,6 +8,7 @@ const ProjectSpecs = props => {
       <p><b>Front end:</b><span>React.js</span></p>
       <p><b>Back end:</b><span>Ruby on Rails</span></p>
       <p>View the <a href='http://bridgetro.se'>user stories</a>, or <a href='http://bridgetro.se'>code</a>.</p>
+      <p className='project-summary'>{props.project.text}</p>
     </div>
   )
 }
