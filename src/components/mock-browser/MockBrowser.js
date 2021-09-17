d622466e225400f36a4f2de15ef253cbf95c086c
bridgetrosefitz <bridgetrosefitz@gmail.com>
2021-09-17 08:04:49 -0400

    finishes mock browser for project snapshots; replaces project snapshots with non-browser ones; adds a few new snapshots to personal portfolio; tidies photo paths for readability

--- src/components/mock-browser/MockBrowser.js
+++ src/components/mock-browser/MockBrowser.js
@@ -4,10 +4,18 @@ import './mock-browser.css'
 const MockBrowser = ({image, altText}) => {
   
   return(
-    <div className='mock-browser-outline'>
-      <img className='project-image' alt={altText} src={image} />
+      <div className='mock-browser-outline'>
+        <div className='mock-browser-top'>
+        {/* <div className='mock-browser-circles-container'> */}
+            <div className='mock-browser-circle'></div>
+            <div className='mock-browser-circle'></div>
+            <div className='mock-browser-circle'></div>
+        {/* </div> */}
+          
+        </div>
+        <img className='project-image' alt={altText} src={image} />
 
-    </div>
+      </div>
   )
 
 }
