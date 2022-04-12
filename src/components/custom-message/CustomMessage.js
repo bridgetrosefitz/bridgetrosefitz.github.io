6cd57a7917a3cb35b7b3c142d873645e88dcc89e
bridgetrosefitz <bridgetrosefitz@gmail.com>
2022-04-12 16:09:00 +1000

    playing with styling of animation; slight tweaks to animation of cusom message

--- src/components/custom-message/CustomMessage.js
+++ src/components/custom-message/CustomMessage.js
@@ -3,37 +3,39 @@ import './custom-message.css'
 import { useSpring, animated, config } from 'react-spring'
 
 const CustomMessage = () => {
-  const [flip, setFlip] = useState(false)
   const [sentenceIndex, setSentenceIndex] = useState(0)
   const [maxSentenceIndex, setMaxSentenceIndex] = useState(1)
 
-  // Use messageIndex to determine which sentence shows up
-  // Get the animation to run once only
-
+  // If you add a new sentence, remember to update maxSentenceIndex
   const sentences = [
-    "Hi, Notion",
-    "Welcome to my site"
+    "Hi, Notion.",
+    "Welcome to my site.",
+    "It's lovely to have you here.",
+    "You can browse my projects below.",
+    "And see my process to build them, including user stories and ERDs.",
+    "I taught myself React-Three-Fiber (Three.js) to make this site.",
+    "I'd love to chat more about how I built it.",
+    "Just let me know :)",
+    "Most of all, I hope you enjoy having a look around.",
   ]
 
   const props = useSpring({
     to: { opacity: 1 }, 
-    from: { opacity: 0 }, 
-    reverse: flip,
-    delay: 100,
+    from: { opacity: 0 },
+    loop: {reverse: true},
     config: config.molasses,
-    onRest: () => setFlip(!flip)
   })
 
   useEffect(() => {
     const interval = setInterval(() => setSentenceIndex(prevSentenceIndex => {
-      return prevSentenceIndex <= maxSentenceIndex ? prevSentenceIndex + 1 : prevSentenceIndex}), 3000)
+      return prevSentenceIndex <= maxSentenceIndex ? prevSentenceIndex + 1 : prevSentenceIndex}), 5000)
     return () => clearInterval(interval)
   }, [])
 
 
   return (
     <div className='custom-message-container'>
-      <animated.div className='custom-message' style={props}>{sentences[sentenceIndex]}</animated.div>
+      <animated.div className={`custom-message ${sentenceIndex > maxSentenceIndex ? 'custom-message-hide' : ''}`} style={props}>{sentences[sentenceIndex]}</animated.div>
     </div>
   )
 }
