7798e023331842d9a8b19f226c91b39bc53e86c4
bridgetrosefitz <bridgetrosefitz@gmail.com>
2022-04-21 08:20:35 +1000

    fixes problem from using ResizeObserve on inline element

--- src/components/custom-message/CustomMessage.js
+++ src/components/custom-message/CustomMessage.js
@@ -3,25 +3,27 @@ import './custom-message.css'
 import { useSpring, animated, config } from 'react-spring'
 
 const useWidth = () => {
+  const initialWidth = 0
   const ref = useRef(null)
-  const [width, setWidth] = useState(0)
-  const widthRef = useRef(width)
+  const [width, setWidth] = useState(initialWidth)
+  const widthRef = useRef(initialWidth)
   const [resizeObserver] = useState(
-    () => 
+    () =>
       new ResizeObserver(packet => {
-        if(ref.current && widthRef.current !== ref.current.offsetWidth) {
-          widthRef.current = ref.current.offsetWidth
-          setWidth(ref.current.offsetWidth)
+        if (ref.current && widthRef.current !== ref.current.offsetWidth) {
+          console.log('inside observer width:', ref.current.offsetWidth)
+          widthRef.current = ref.current.offsetWidth;
+          setWidth(ref.current.offsetWidth);
         }
       })
-  )
+  );
 
   useLayoutEffect(() => {
     if(ref.current) {
+      resizeObserver.observe(ref.current, {});
       setWidth(ref.current.offsetWidth)
-      resizeObserver.observe(ref.current, {})
     }
-    return () => resizeObserver.disconnect()
+    return () => resizeObserver.disconnect();
   }, [ref.current])
 
   return [ref, width]
@@ -31,7 +33,7 @@ const CustomMessage = () => {
   const [sentenceIndex, setSentenceIndex] = useState(0)
   const [maxSentenceIndex, setMaxSentenceIndex] = useState(8)
   const [widthRef, width] = useWidth()
-
+  console.log(width)
   const props = useSpring({
     from: { opacity: 0, width: 0 },
     to: { opacity: 1, width: width },
@@ -52,7 +54,7 @@ const CustomMessage = () => {
   // If you add a new sentence, remember to update maxSentenceIndex
 
   const sentences = [
-    `Hi, ${generateNameForMessage(window.location.href)}.`,
+    // `Hi, ${generateNameForMessage(window.location.href)}.`,
     "Welcome to my site.",
     "It's lovely to have you here.",
     "You can browse my projects below.",
@@ -69,16 +71,18 @@ const CustomMessage = () => {
     return () => clearInterval(interval)
   }, [])
 
-
+  if(widthRef.current) {
+    console.log('Width REF:', widthRef.current.offsetWidth)
+  }
   return (
     <div className='custom-message-container'>
         <animated.div 
           className={`custom-message ${sentenceIndex > maxSentenceIndex ? 'custom-message-hide' : ''}`} 
-          style={{...props, overflow: 'hidden'}}
+        style={{ ...props, overflow: 'hidden'}}
         >
-          <div ref={widthRef}>
-            {sentences[0]}
-          </div>
+          <span className='custom-message-text' ref={widthRef}>
+            {sentences[sentenceIndex]}
+          </span>
         </animated.div>
     </div>
   )
