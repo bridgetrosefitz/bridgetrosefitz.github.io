32b7c43190789475f50cc9410c8be3e782dd2684
bridgetrosefitz <bridgetrosefitz@gmail.com>
2022-04-28 12:06:04 +1000

    implements translations for custom message

--- src/components/custom-message/CustomMessage.js
+++ src/components/custom-message/CustomMessage.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import './custom-message.css'
 import { useSpring, animated, config } from 'react-spring'
+import { useTranslation } from 'react-i18next'
+import i18next from 'i18next'
 
 const CustomMessage = () => {
+  const { t } = useTranslation()
   const [sentenceIndex, setSentenceIndex] = useState(0)
   const maxSentenceIndex = 9
 
@@ -40,23 +43,35 @@ const CustomMessage = () => {
     if(allowableNames.includes(lowerCase)) {
       const properCase = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1)
       return properCase
-    } else return "there"
+    } else return t('custom-message.there')
   }
 
   // If you add a new sentence, remember to update maxSentenceIndex
-
   const sentences = [
-    `Hi, ${generateNameForMessage(window.location.href)}.`,
-    "Welcome to my site.",
-    "It's lovely to have you here.",
-    "You can browse my projects below.",
-    "And see my process to build them, \n including user stories and ERDs.",
-    "I taught myself \n React-Three-Fiber (Three.js) \n to make this site.",
-    "I'd love to chat more \n about how I built it.",
-    "Just let me know :)",
-    "Most of all, \n I hope you enjoy having \n a look around.",
-    "Bridget x"
+    `${t('custom-message.Hi')} ${generateNameForMessage(window.location.href)}`,
+    `${t("custom-message.Welcome to my site")}`,
+    `${t("custom-message.It's lovely to have you here")}`,
+    `${t("custom-message.You can browse my projects below")}`,
+    `${t("custom-message.And see my process to build them, including user stories and ERDs")}`,
+    `${t("custom-message.I taught myself React-Three-Fiber (Three.js) to make this site")}`,
+    `${t("custom-message.I'd love to chat more about how I built it")}`,
+    `${t("custom-message.Just let me know")}`,
+    `${t("custom-message.Most of all, I hope you enjoy having a look around")}`,
+    `${t("custom-message.Bridget x")}`
   ]
+
+  // const sentences = [
+  //   `Hi, ${generateNameForMessage(window.location.href)}.`,
+  //   "Welcome to my site.",
+  //   "It's lovely to have you here.",
+  //   "You can browse my projects below.",
+  //   "And see my process to build them, \n including user stories and ERDs.",
+  //   "I taught myself \n React-Three-Fiber (Three.js) \n to make this site.",
+  //   "I'd love to chat more \n about how I built it.",
+  //   "Just let me know :)",
+  //   "Most of all, \n I hope you enjoy having \n a look around.",
+  //   "Bridget x"
+  // ]
 
   return (
     <div className='custom-message-container'>
